7918d0e0f8a2afbe7dcadcafb549e12abbbf0f60
Rafael Zalamena <rzalamena@opensourcerouting.org>
2024-07-25 22:46:20 -0300

    config,telescope: import version 0.1.8

--- init.lua
+++ init.lua
@@ -15,3 +15,13 @@ require("catppuccin").setup({
 })
 
 vim.cmd.colorscheme("catppuccin")
+
+-- Enable telescope for a set of tools for fuzzy searching with a nice
+-- interface
+require("telescope").setup({})
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
